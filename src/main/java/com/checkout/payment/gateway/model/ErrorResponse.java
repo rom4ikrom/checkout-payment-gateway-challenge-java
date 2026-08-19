35fd19aa58ab5900a9261e8c5e8fe9748ddb0c18
Roman Romanov <roman.romanov1998@gmail.com>
2026-08-19 12:05:49 +0100

    use immutable records for dtos

--- src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
+++ src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
@@ -1,20 +1,5 @@
 package com.checkout.payment.gateway.model;
 
-public class ErrorResponse {
-  private final String message;
+public record ErrorResponse(String message) {
 
-  public ErrorResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  @Override
-  public String toString() {
-    return "ErrorResponse{" +
-        "message='" + message + '\'' +
-        '}';
-  }
 }
