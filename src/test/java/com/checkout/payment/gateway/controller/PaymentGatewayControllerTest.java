92649e27d5617fa9bd1c059e6069330d1d127dea
Roman Romanov <roman.romanov1998@gmail.com>
2026-08-19 12:42:01 +0100

    update spring boot and gradle

--- src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
+++ src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
@@ -10,8 +10,8 @@ import com.checkout.payment.gateway.repository.PaymentsRepository;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
